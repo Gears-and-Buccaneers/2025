5924308f9172e2dccf3b1b1afbcd23c85f166e7c
Deven Speyrer <devenspeyrer@gmail.com>
2025-02-11 22:59:23 -0700

    Add NetworkTables publisher for PhotonVision pose estimate

--- src/main/java/frc/robot/subsystems/AprilTags.java
+++ src/main/java/frc/robot/subsystems/AprilTags.java
@@ -8,12 +8,16 @@ import com.ctre.phoenix6.Utils;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class AprilTags implements Subsystem {
     private final PhotonCamera m_camera;
     private final PhotonPoseEstimator m_photonPoseEstimator;
+    private final StructPublisher<Pose3d> posePublisher = NetworkTableInstance.getDefault().getStructTopic("PhotonVision Pose Estimate", Pose3d.struct).publish();
 
     private final Swerve m_drivetrain;
 
@@ -37,6 +41,7 @@ public class AprilTags implements Subsystem {
 
             if (estimate.isPresent()) {
                 var est = estimate.get();
+                posePublisher.set(est.estimatedPose);
                 m_drivetrain.addVisionMeasurement(est.estimatedPose.toPose2d(),
                         Utils.fpgaToCurrentTime(est.timestampSeconds));
             }
