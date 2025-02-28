18e05de8e32838597790b7891592b07fa2ceb00f
M1stersushiguy <161532371+M1stersushiguy@users.noreply.github.com>
2025-02-27 18:06:29 -0700

    a wittle tiny LED change ;)

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -1,12 +1,17 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.led.Animation;
 import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.RainbowAnimation;
+
 
 public class LEDs {
     private final CANdle candle;
+    Animation b = new RainbowAnimation(1, .3, -1, false, 8);
 
     public LEDs(int channel) {
         this.candle = new CANdle(channel, "drivetrain");
-        this.candle.setLEDs(0, 54, 140);
+
+        candle.animate(b);
     }
 }
