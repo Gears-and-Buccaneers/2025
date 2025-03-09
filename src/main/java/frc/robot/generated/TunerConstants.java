30fd4fce1a9cc43764a832b2085558baf592a0ab
Deven Speyrer <devenspeyrer@gmail.com>
2025-03-09 14:11:14 -0600

    Unretune Swerve steer motors

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -25,7 +25,7 @@ public class TunerConstants {
     // the
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains = new Slot0Configs()
-            .withKP(3000).withKI(0).withKD(78)
+            .withKP(1600).withKI(0).withKD(57)
             .withKS(0.1).withKV(0).withKA(0)
             .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
     // When using closed-loop control, the drive motor uses the control
